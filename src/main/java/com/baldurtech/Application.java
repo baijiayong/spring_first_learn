936ee51852724e8a808507e95bc60b1386578e99
baijiayong <jiayongbai@gmail.com>
2014-09-15 11:23:46 +0800

    实现了简单的web页面,/contact和/contact/list时的输出

--- src/main/java/com/baldurtech/Application.java
+++ src/main/java/com/baldurtech/Application.java
@@ -2,13 +2,17 @@ package com.baldurtech;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
 
 @Configuration
 @ComponentScan
+@EnableAutoConfiguration
 public class Application { 
     public static void main(String args[]) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         ContactController printer = context.getBean(ContactController.class);
+        SpringApplication.run(Application.class, args);
         printer.printMessage();
     }
 }
