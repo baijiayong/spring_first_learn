ebad662432c93d6a7878552754e984257d5344b8
baijiayong <jiayongbai@gmail.com>
2014-09-17 10:12:41 +0800

    修改了测试 SiteIntegrationTest ，让测试通过

--- src/test/java/com/baldurtech/controller/SiteIntegrationTest.java
+++ src/test/java/com/baldurtech/controller/SiteIntegrationTest.java
@@ -3,18 +3,22 @@ package com.baldurtech.controller;
 import com.baldurtech.core.ContactService;
 import com.baldurtech.events.RequestAllContactItemsEvent;
 
+import static org.hamcrest.Matchers.is;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class SiteIntegrationTest {
@@ -38,6 +42,8 @@ public class SiteIntegrationTest {
     public void thatTextReturned() throws Exception {
         mockMvc.perform(get("/"))
             .andDo(print())
-            .andExpect(content().string("foo!"));
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("contactList"))
+            .andExpect(view().name(is("list")));
     }
 }
